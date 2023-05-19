d93fe65be48b9004c5d16164f60fc75c1f77ecfd
Santiago Rodríguez <srodriguezm95@gmail.com>
2023-05-19 12:40:40 -0500

    Validation on UI

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -1,7 +1,7 @@
 export const Quote = ({ quote }: { quote: string }) => {
 
   return <div class="relative">
-    <div class="absolute w-1 h-full bg-yellow-500"></div>
+    <div class="absolute w-1 h-full bg-yellow-500 rounded-sm"></div>
     <p class="pl-24 text-[36px]">“{quote}”</p>
   </div>
 };
